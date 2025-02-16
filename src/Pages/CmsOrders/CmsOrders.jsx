81956f92ab5fb64cf89892359a26d6ed9a563ab4
Milad Rabiei <rabiei.milad32@gmail.com>
2025-02-16 19:10:02 +0330

    completed orders route in cms

--- src/Pages/CmsOrders/CmsOrders.jsx
+++ src/Pages/CmsOrders/CmsOrders.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function CmsOrders() {
   return (
-    <div>CmsOrders</div>
+    <section className="cmsOrders mt-8">
+      <h2 className='font-MorabbaMedium text-3xl'>سفارش ها</h2>
+      <div className="flex-center mt-4 mb-8 rounded-lg bg-white p-5 border border-gray-300 h-[350px]">
+        <h2 className='font-MorabbaMedium text-3xl'>هنوز هیچ سفارشی ثبت نشده است!</h2>
+      </div>
+    </section>
   )
 }
